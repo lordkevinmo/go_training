947f6056660c7c493d1b1d107081b3a794270f96
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-08-31 21:13:42 +0200

    add post and put verb handlers

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -32,9 +33,60 @@ func (p *Products) ToJSON(w io.Writer) error {
 	return e.Encode(p)
 }
 
+// FromJSON deserializes the contents of the JSON to Product object
+// NewDecoder provides better performance than json.Marshal as it does not
+// have to buffer the output into an in memory slice of bytes
+// this reduces allocations and the overheads of the service
+//
+// https://golang.org/pkg/encoding/json/#NewDecoder
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
+}
+
 // GetProducts returns a list of products
 func GetProducts() Products {
 	return productList
+}
+
+// AddProduct add new product object to the productlist
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+// UpdateProduct update an existing product with ID: id
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+// Return the next ID of the product based on the ID value of the last product
+// in the product list
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+// ErrProductNotFound is a custom error thrown when the product searched by the
+// user is not found
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+// Find the product by its ID
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
 }
 
 // productList is a hard coded list of products for this
--- handlers/products.go
+++ handlers/products.go
@@ -4,12 +4,16 @@ import (
 	"classifieds/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
+// Products is a type defined to represent product
 type Products struct {
 	l *log.Logger
 }
 
+// NewProducts defined an DI to log events occured in product api
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
@@ -27,6 +31,36 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Handle the request for editing an existing product
+	if r.Method == http.MethodPut {
+		p.l.Println("Handle PUT request", r.URL.Path)
+
+		// expect the ID in the URI
+		reg := regexp.MustCompile(`([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		id := g[0][1]
+		idString, err := strconv.Atoi(id)
+
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(idString, rw, r)
+		return
+	}
+
 	// catch all.
 	// if no method is specified, return error
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -34,6 +68,15 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle POST product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, req *http.Request) {
@@ -46,5 +89,28 @@ func (p *Products) getProducts(rw http.ResponseWriter, req *http.Request) {
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to Marshal JSON", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
 	}
 }
